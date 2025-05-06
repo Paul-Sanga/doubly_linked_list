6fa6f9fe3e072ea93416ff3d409d8a9c6ce89492
Paul Sanga Nyamawi <paul.nyamawi@griffinglobaltech.com>
2025-05-06 17:07:27 +0300

    [Added the add_before and find_by_index_from_tail functions]

--- src/lib.rs
+++ src/lib.rs
@@ -77,29 +77,104 @@ impl DoublyLinkedList {
             self.tail = Some(Rc::downgrade(&node));
             self.head = Some(node);
         } else {
-            self.tail.as_ref().and_then(|tail| tail.upgrade()).map(|tail|{
-                node.borrow_mut().prev = Some(Rc::downgrade(&tail));
-                tail.borrow_mut().next = Some(Rc::clone(&node))
-            });
+            self.tail
+                .as_ref()
+                .and_then(|tail| tail.upgrade())
+                .map(|tail| {
+                    node.borrow_mut().prev = Some(Rc::downgrade(&tail));
+                    tail.borrow_mut().next = Some(Rc::clone(&node))
+                });
             self.tail = Some(Rc::downgrade(&node));
         }
         self.size += 1;
     }
 
     pub fn remove_first(&mut self) -> Result<(), String> {
-        if self.size > 1 {
+        if self.size == 1 {
+            self.head = None;
+            self.tail = None;
+        } else if self.size > 1 {
             if let Some(old_head) = self.head.take() {
                 if let Some(new_head) = old_head.borrow_mut().next.take() {
                     self.head = Some(Rc::clone(&new_head))
                 }
             }
-        } else if self.size == 1 {
-            self.head = None;
-            self.tail = None;
         } else {
             return Err("Can not remove from an empty list".to_string());
         }
         self.size -= 1;
         Ok(())
+    }
+
+    pub fn remove_last(&mut self) -> Result<(), String> {
+        if self.size == 1 {
+            self.head = None;
+            self.tail = None;
+        } else if self.size > 1 {
+            if let Some(old_tail) = self.tail.take() {
+                if let Some(old_tail) = old_tail.upgrade() {
+                    if let Some(new_tail) = old_tail.borrow_mut().prev.take() {
+                        if let Some(new_tail) = new_tail.upgrade() {
+                            self.tail = Some(Rc::downgrade(&new_tail));
+                        }
+                    }
+                }
+            }
+        } else {
+            return Err("Can not remove from an empty list".to_string());
+        }
+        self.size -= 1;
+        Ok(())
+    }
+
+    pub fn add_before(&mut self, seek: u32, value: u32) -> Result<(), String> {
+        let mut current_opt = self.head.clone();
+        while let Some(current_node) = current_opt {
+            if current_node.borrow().value == seek {
+                let new_node = Rc::new(RefCell::new(Node::new(value)));
+                new_node.borrow_mut().next = Some(Rc::clone(&current_node));
+                if let Some(prev_node) = current_node.borrow().prev.as_ref() {
+                    if let Some(prev_node) = prev_node.upgrade() {
+                        prev_node.borrow_mut().next = Some(Rc::clone(&new_node));
+                        new_node.borrow_mut().prev = Some(Rc::downgrade(&prev_node));
+                    }
+                } else {
+                    self.head = Some(Rc::clone(&new_node));
+                }
+                current_node.borrow_mut().prev = Some(Rc::downgrade(&new_node));
+                self.size += 1;
+                return Ok(());
+            }
+            current_opt = current_node
+                .borrow()
+                .next
+                .as_ref()
+                .map(|node| Rc::clone(node));
+        }
+        Err(format!("Value {seek} not found in list"))
+    }
+
+    pub fn find_by_index_from_tail(&self, index: usize) -> Option<u32> {
+        let mut iteration = 0usize;
+        let mut current_opt = self.tail.clone();
+        while let Some(current_node) = current_opt.as_ref() {
+            if iteration == index {
+                return current_node
+                    .upgrade()
+                    .as_ref()
+                    .map(|node| node.borrow().value);
+            } else {
+                if let Some(current_node) = current_node.upgrade() {
+                    current_opt = current_node
+                        .borrow()
+                        .prev
+                        .as_ref()
+                        .and_then(|node| node.upgrade())
+                        .map(|node| Rc::downgrade(&node));
+                    iteration += 1
+                }
+            }
+        }
+        None
     }
 }
--- src/test.rs
+++ src/test.rs
@@ -1,3 +1,5 @@
+use std::io::Lines;
+
 #[cfg(test)]
 use super::*;
 
@@ -31,6 +33,9 @@ fn test_remove_first() {
     list.add_last(6);
     list.add_last(7);
     list.add_last(8);
+    assert_eq!(list.head(), Some(5));
+    assert_eq!(list.tail(), Some(8));
+    assert_eq!(list.size(), 4);
     list.remove_first().unwrap();
     list.remove_first().unwrap();
     assert_eq!(list.head(), Some(7));
@@ -44,5 +49,56 @@ fn test_remove_first() {
     assert_eq!(
         list.remove_first(),
         Err("Can not remove from an empty list".to_string())
-    )
+    );
+}
+
+#[test]
+fn test_remove_last() {
+    let mut list = DoublyLinkedList::new();
+    list.add_first(5);
+    list.add_first(6);
+    list.add_first(7);
+    assert_eq!(list.size(), 3);
+    assert_eq!(list.head(), Some(7));
+    assert_eq!(list.tail(), Some(5));
+    list.remove_last().unwrap();
+    assert_eq!(list.size(), 2);
+    assert_eq!(list.head(), Some(7));
+    assert_eq!(list.tail(), Some(6));
+    list.remove_last().unwrap();
+    assert_eq!(list.size(), 1);
+    assert_eq!(list.head(), Some(7));
+    assert_eq!(list.tail(), Some(7));
+    list.remove_last().unwrap();
+    assert_eq!(list.size(), 0);
+    assert_eq!(list.head(), None);
+    assert_eq!(list.tail(), None);
+    assert_eq!(
+        list.remove_last(),
+        Err("Can not remove from an empty list".to_string())
+    );
+}
+
+#[test]
+fn test_find_by_index_from_tail() {
+    let mut list = DoublyLinkedList::new();
+    list.add_first(5);
+    list.add_first(6);
+    list.add_first(7);
+    assert_eq!(list.find_by_index_from_tail(2), Some(7));
+    assert_eq!(list.find_by_index_from_tail(1), Some(6));
+    assert_eq!(list.find_by_index_from_tail(0), Some(5));
+    assert_eq!(list.find_by_index_from_tail(3), None);
+}
+
+#[test]
+fn test_add_before(){
+    let mut list = DoublyLinkedList::new();
+    list.add_last(5);
+    list.add_last(6);
+    list.add_last(7);
+    list.add_last(8);
+    list.add_before(5, 10).unwrap();
+    assert_eq!(list.size(), 5);
+    assert_eq!(list.head(), Some(10));
 }
